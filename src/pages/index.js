d0629ef817f51e939e5668fb3441dd9d205c58da
mediavil <meekeboose@gmail.com>
2020-05-25 15:56:56 +0800

    styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import React from "react"
 import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
 
 const index = () => {
   return (
     <Layout>
-      <h1>This is the homepage</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
-        voluptatum nam nemo
-      </p>
+      <div>
+        <h1>This is the homepage</h1>
+        <ExampleButton>Click me</ExampleButton>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+          voluptatum nam nemo
+        </p>
+      </div>
     </Layout>
   )
 }
